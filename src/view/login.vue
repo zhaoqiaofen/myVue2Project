02dc1b5c9625a61f410f942a4b5ea9d4f46730eb
DESKTOP-HDK2FVE\ZH <1872116275@qq.com>
2021-07-27 22:39:08 +0800

    fix: 测试

--- src/view/login.vue
+++ src/view/login.vue
@@ -136,7 +136,7 @@ export default {
             }
           })
         } else {
-          console.log('error submit!!')
+          console.log('error submit')
           return false
         }
       })
